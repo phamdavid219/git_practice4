0cefd9e65cc10e4763fc09ddb041241e4dc3656e
CUONG PHAM <phamdavid219@gmail.com>
2022-01-24 10:38:16 -0500

    2nd work is done

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,6 @@ package hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("B25 Learning Git and Github");
     }
 }
